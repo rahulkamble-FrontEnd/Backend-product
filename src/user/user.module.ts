7ec85e3bb91a22cd47afb65f94f51770c5a798e4
Rahul Kamble <rahulkamble@customfurnish.com>
2026-04-10 16:48:08 +0530

    designer_notes table

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { DesignerNote } from './designer-note.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, DesignerNote])],
   providers: [UserService],
   exports: [UserService],
   controllers: [UserController],
